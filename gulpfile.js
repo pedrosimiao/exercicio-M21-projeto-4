e95dffbecd8b1c65b100ed7434024bd3742b5e57
pedrosimiao <pedrolumiere@gmail.com>
2024-01-22 04:50:06 -0300

    gulp test initial config

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles() {
+    return gulp.src('./src/styles/*scss')
+    .pipe(sass({ outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dest/styles/css'));
+}
+
+// function testGulp(cb) {
+//     console.log('Tô funfando');
+//     cb();
+// }
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*scss', gulp.parallel(styles)); 
+}
+// exports.default = testGulp;
